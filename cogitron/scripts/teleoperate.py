ec1e7fd125f27194abe444234527caa785c13eca
ChristianGynnild <75206913+ChristianGynnild@users.noreply.github.com>
2025-03-22 18:40:44 +0100

    teleoperate script

---
+++ cogitron/scripts/teleoperate.py
@@ -0,0 +1,8 @@
+from cogitron.arms import robot
+import tqdm
+
+seconds = 30
+frequency = 200
+for _ in tqdm.tqdm(range(seconds*frequency)):
+    leader_pos = robot.leader_arms["main"].read("Present_Position")
+    robot.follower_arms["main"].write("Goal_Position", leader_pos)
