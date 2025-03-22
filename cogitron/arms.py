1199ef8e8143b014347cad1d349c6559d1c0dc14
thoab <thomas.adrian.berg@gmail.com>
2025-03-22 15:27:10 +0100

    added arms.py

---
+++ cogitron/arms.py
@@ -0,0 +1,43 @@
+import sys
+import os
+sys.path.append(os.path.abspath("."))
+
+
+from lerobot.common.robot_devices.motors.dynamixel import DynamixelMotorsBus
+
+from time import sleep
+from numpy import array, int32
+import numpy as np
+
+from lerobot.common.robot_devices.motors.dynamixel import TorqueMode
+
+
+leader_port = "/dev/ttyACM1"
+follower_port = "/dev/ttyACM0"
+
+leader_arm = DynamixelMotorsBus(
+    port=leader_port,
+    motors={
+        # name: (index, model)
+        "shoulder_pan": (1, "xl330-m077"),
+        "shoulder_lift": (2, "xl330-m077"),
+        "elbow_flex": (3, "xl330-m077"),
+        "wrist_flex": (4, "xl330-m077"),
+        "wrist_roll": (5, "xl330-m077"),
+        "gripper": (6, "xl330-m077"),
+    },
+)
+
+
+follower_arm = DynamixelMotorsBus(
+    port=follower_port,
+    motors={
+        # name: (index, model)
+        "shoulder_pan": (1, "xl430-w250"),
+        "shoulder_lift": (2, "xl430-w250"),
+        "elbow_flex": (3, "xl330-m288"),
+        "wrist_flex": (4, "xl330-m288"),
+        "wrist_roll": (5, "xl330-m288"),
+        "gripper": (6, "xl330-m288"),
+    },
+)
