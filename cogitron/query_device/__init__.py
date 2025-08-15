6bb69585dffc0eac1dfaa675ae98431f28c7218e
ChristianGynnild <75206913+ChristianGynnild@users.noreply.github.com>
2025-08-15 16:20:16 +0200

    Check if idVendor and idProduct files exists before trying to open them

--- cogitron/query_device/__init__.py
+++ cogitron/query_device/__init__.py
@@ -6,6 +6,8 @@ def get_device_id(device_filepath:str):
     Get the unique vendor id and product id for an device.
 
     returns a string in the form of: "vendor_id:product_id"
+
+    If the program is unable to find the id, return None.
     """
     
     if not os.path.exists(device_filepath):
@@ -19,10 +21,16 @@ def get_device_id(device_filepath:str):
     vendor_id_path = os.path.join(path, "idVendor")
     product_id_path = os.path.join(path, "idProduct")
 
-    with open(vendor_id_path) as f:
-        vendor_id = f.read().replace("\n", "")
+    if os.path.exists(vendor_id_path):
+        with open(vendor_id_path) as f:
+            vendor_id = f.read().replace("\n", "")
+    else:
+        return None
 
-    with open(product_id_path) as f:
-        product_id = f.read().replace("\n", "")
+    if os.path.exists(product_id_path):
+        with open(product_id_path) as f:
+            product_id = f.read().replace("\n", "")
+    else:
+        return None
 
     return f"{vendor_id}:{product_id}"
