64b5e14406c0575d58abb5ee0aaf27eefc6d25aa
ChristianGynnild <75206913+ChristianGynnild@users.noreply.github.com>
2025-08-17 11:22:25 +0200

    Sleep after reboot of motors

--- src/teleoperate.py
+++ src/teleoperate.py
@@ -1,10 +1,13 @@
 from cogitron.arms import get_follower_arm, get_leader_arm, reboot
+import time
 
 robot = get_follower_arm()
 teleop_device = get_leader_arm()
 
 reboot(robot.bus.port_handler)
 reboot(teleop_device.bus.port_handler)
+
+time.sleep(1)
 
 robot.connect()
 teleop_device.connect()
