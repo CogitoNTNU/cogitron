f0c95952ac2fad1a8f91adcce924ac7b85e0d29d
ChristianGynnild <75206913+ChristianGynnild@users.noreply.github.com>
2025-08-24 16:41:20 +0200

    Turn ints into str before concat

--- cogitron/scripts/get_camera_config.py
+++ cogitron/scripts/get_camera_config.py
@@ -2,8 +2,8 @@ import cogitron
 
 def main():
     camera_config = cogitron.camera.get_camera_config()
-    print('"{ front: {type: opencv, index_or_path: ' + camera_config.index_or_path \
-          + ', width: ' + camera_config.width + ', height: ' + camera_config.height + ', fps: ' + camera_config.fps + '}}"')
+    print('"{ front: {type: opencv, index_or_path: ' + str(camera_config.index_or_path) \
+          + ', width: ' + str(camera_config.width) + ', height: ' + str(camera_config.height) + ', fps: ' + str(camera_config.fps) + '}}"')
 
 if __name__ == '__main__':
     main()
