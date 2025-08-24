ffff0ce2118555d5508e236c811f0ed93a5bf8e9
ChristianGynnild <75206913+ChristianGynnild@users.noreply.github.com>
2025-08-24 17:45:58 +0200

    Fix syntax errors in record_dataset.sh

--- cogitron/scripts/get_camera_config.py
+++ cogitron/scripts/get_camera_config.py
@@ -19,7 +19,7 @@ def main():
         camera_config = cogitron.camera.get_camera_config()
 
     print('"{ front: {type: opencv, index_or_path: ' + str(camera_config.index_or_path) \
-          + ', width: ' + str(camera_config.width) + ', height: ' + str(camera_config.height) + ', fps: ' + str(camera_config.fps) + '}}"')
+          + ', width: ' + str(camera_config.width) + ', height: ' + str(camera_config.height) + ', fps: ' + str(int(camera_config.fps)) + '}}"')
 
 if __name__ == '__main__':
     main()
--- cogitron/scripts/record_dataset.sh
+++ cogitron/scripts/record_dataset.sh
@@ -1,14 +1,12 @@
-camera_config=$(cogitron-camera-config);
-
-lerobot-record \
+echo lerobot-record \
     --robot.type=koch_follower \
     --robot.port=$(cogitron-follower-port) \
     --robot.id=follower_arm \
-    --robot.cameras=$camera_config \
+    --robot.cameras=$(cogitron-camera-config) \
     --teleop.type=koch_leader \
-    --teleop.port=$(cogitron-leader-port 2> /dev/null) \
+    --teleop.port=$(cogitron-leader-port) \
     --teleop.id=leader_arm \
     --display_data=true \
     --dataset.repo_id=${HF_USER}/record-test \
     --dataset.num_episodes=5 \
-    --dataset.single_task=$1
+    --dataset.single_task=$1 | bash
